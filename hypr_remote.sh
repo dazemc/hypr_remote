4f4aa2e06b1af1dc523813e22f5f0c2182f806c9
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-09-13 20:30:23 -0700

    service: move to user-scope systemd with headless lifecycle

--- hypr_remote.sh
+++ hypr_remote.sh
@@ -1,57 +1,30 @@
 #!/usr/bin/env bash
 
-get_var_from_proc() {
-  local proc_name="$1"
-  local var_name="$2"
-
-  for pid in $(pgrep -u "$USER" "$proc_name"); do
-    val=$(tr '\0' '\n' < /proc/$pid/environ 2>/dev/null | grep "^$var_name=" | cut -d= -f2-)
-    if [[ -n "$val" ]]; then
-      echo "$val"
-      return 0
-    fi
-  done
-
-  return 1
-}
-
-if [[ -z "$HYPRLAND_INSTANCE_SIGNATURE" || -z "$WAYLAND_DISPLAY" ]]; then
-  echo "[ERROR] Missing Hyprland environment variables."
-  exit 1
-fi
-
-sudo sh -c "
-echo "HYPRLAND_INSTANCE_SIGNATURE=$HYPRLAND_INSTANCE_SIGNATURE" > /etc/hyprland_env;
-echo "WAYLAND_DISPLAY=$WAYLAND_DISPLAY" >> /etc/hyprland_env;
-"
-
 VIRTUAL_MONITOR="HEADLESS-2"
-VIRTUAL_WORKSPACE=3
-REAL_MONITOR="DP-2"
+VIRTUAL_WORKSPACE=10
+REAL_MONITOR="HDMI-A-1"
 
 cleanup() {
+  pkill wayvnc
   hyprctl dispatch moveworkspacetomonitor "$VIRTUAL_WORKSPACE" "$REAL_MONITOR"
   hyprctl dispatch focusmonitor "$REAL_MONITOR"
-  pkill wayvnc
-  exit 0
 }
 
 trap cleanup INT TERM EXIT
 
-if ! hyprctl monitors | grep -q "$VIRTUAL_MONITOR"; then
-  echo "[wayvnc] Creating $VIRTUAL_MONITOR dynamically..."
-  hyprctl output create headless
+main() {
+  hyprctl output remove "$VIRTUAL_MONITOR" 2>/dev/null
+  sleep 0.2
+  hyprctl output create headless "$VIRTUAL_MONITOR"
   sleep 0.5
-fi
 
-echo "Workspace $VIRTUAL_WORKSPACE to $VIRTUAL_MONITOR..."
-hyprctl dispatch moveworkspacetomonitor "$VIRTUAL_WORKSPACE" "$VIRTUAL_MONITOR"
-sleep 0.2
-hyprctl dispatch workspace "$VIRTUAL_WORKSPACE"
-sleep 0.2
+  hyprctl dispatch workspace "$VIRTUAL_WORKSPACE"
+  hyprctl dispatch moveworkspacetomonitor "$VIRTUAL_WORKSPACE" "$VIRTUAL_MONITOR"
+  sleep 0.2
 
-hyprctl dispatch focusmonitor "$REAL_MONITOR"
+  hyprctl dispatch focusmonitor "$VIRTUAL_MONITOR"
 
-echo "Started WayVNC on $VIRTUAL_MONITOR..."
-wayvnc 0.0.0.0 5900 "$VIRTUAL_MONITOR"
+  wayvnc 0.0.0.0 5900 "$VIRTUAL_MONITOR"
+}
 
+main
--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+mkdir -p ~/.config/systemd/user/
+mkdir -p ~/.local/bin/
 cp ./hypr_remote.service ./hypr_remote.service.working
 sed -i "s/RESU/$USER/g" ./hypr_remote.service.working
 sed -i "s|GDX|$XDG_RUNTIME_DIR|g" ./hypr_remote.service.working
-sudo sh -c "cp ./hypr_remote.service.working /etc/systemd/system/hypr_remote.service; cp ./hypr_remote.sh /usr/local/bin/hypr_remote.sh; systemctl daemon-reload"
-
+cp ./hypr_remote.service.working ~/.config/systemd/user/hypr_remote.service
+cp ./hypr_remote.sh ~/.local/bin/hypr_remote.sh
+systemctl --user daemon-reload
